3aaea901eb6212e1bc568763648a7efac644c09d
Nigel Espi <nigel.espi@mb22464.lan>
2017-02-27 20:11:08 +0000

    Added SMTP details to environment variables

--- routes/book.js
+++ routes/book.js
@@ -11,37 +11,37 @@ router.get('/', function(req, res, next) {
 router.post('/request', function(req, res, next) {
 	console.log("POST request made:");
 	console.log("First Name: " + req.body.firstName);
-	//res.layout('layout', {title: 'WSQ - Booking', page_name: 'book'}, {content:{block: 'booking/booking_content'}, specific_css:{block: 'booking/booking_css'}});
-	res.send("success");
+	
+	console.log("SMTP port: " + process.env.SMTP_PORT);
 	// send mail with defined transport object
-	/*transporter.sendMail(mailOptions, (error, info) => {
+	transporter.sendMail(mailOptions, (error, info) => {
 		if (error) {
-			return console.log(error);
+			res.send("error");
 		}
 		console.log('Message %s sent: %s', info.messageId, info.response);
-	});*/
+		res.send("success");
+	});
 });
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.wiredstrings.co.za',
+    port: process.env.SMTP_PORT,
+    secure: false, // upgrade later with STARTTLS
     auth: {
-        user: '',
-        pass: ''
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
 });
 
 // setup email data with unicode symbols
 let mailOptions = {
-    from: '"Info - Wired String Quartet" <info@wiredstrings.co.za>', // sender address
-    to: 'kendrahoofcare@gmail.com', // list of receivers
+    from: '"Anouk - Wired String Quartet" <anouk@wiredstrings.co.za>', // sender address
+    to: 'nigesp@gmail.com', // list of receivers
     subject: 'Booking request - Wired String Quartet', // Subject line
     text: 'Thank you for requesting a booking...', // plain text body
     html: '<b>Thank you for requesting a booking...</b>' // html body
 };
-
-
-
 
 module.exports = router;
 
